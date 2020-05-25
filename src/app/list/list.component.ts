0db072e057e430382c33d9d3bf04609c7d545fe2
mike2611 <mpuentes2611@gmail.com>
2020-05-24 19:08:59 -0500

    E2 AA

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,24 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { AUTOMOVILES } from '../data';
-import {Automovil} from '../models'
+import {Automovil} from '../models';
+import {NgbModalConfig, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+  providers: [NgbModalConfig, NgbModal]
 })
 export class ListComponent implements OnInit {
 
   autos: Automovil[];
-  autoSeleccionado: Automovil;
-  constructor() { }
+  autoMarca: String;
+  autoModelo: String;
+  autoFecha: String;
+  constructor(config: NgbModalConfig, private modalService: NgbModal) 
+  { }
 
   ngOnInit(): void {
     this.autos = AUTOMOVILES;
   }
 
-  onSelect(auto: Automovil){
-    this.autoSeleccionado = auto;
+  onClick(content, auto: Automovil){
+    this.modalService.open(content)
+    
+    this.autoMarca = "Marca: " + auto.marca;
+    this.autoModelo = "Modelo: " + auto.modelo;
+    this.autoFecha = "Fecha de Registro: " + auto.fecha_registro.getDay().toString()+"/"+auto.fecha_registro.getMonth().toString() + "/" + auto.fecha_registro.getFullYear().toString();
+    
   }
 
 }
