360b4a029acfff749111f2d7582ad66d621dd1d3
mike2611 <mpuentes2611@gmail.com>
2020-05-25 16:22:36 -0500

    E3 A7

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {AUTOMOVILES} from  '../data';
+import {Automovil} from '../models';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
+
+
 export class TableComponent implements OnInit {
+
+  autos: Automovil[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.autos = AUTOMOVILES;
+
   }
 
 }
