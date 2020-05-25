2d0eef98775cb2497a72e2252253de27d57f60a2
mike2611 <mpuentes2611@gmail.com>
2020-05-25 15:28:42 -0500

    E3 A6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { TableComponent } from './table/table.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 const routes: Routes = [
+  {
+    path:'',
+    redirectTo: '/table',
+    pathMatch: 'full'
+  },
   {
     path: 'list',
     component: ListComponent
@@ -13,6 +19,10 @@ const routes: Routes = [
   {
     path: 'table',
     component: TableComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { VistasComponent } from './vistas/vistas.component';
 import { ListComponent } from './list/list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TableComponent } from './table/table.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { TableComponent } from './table/table.component';
     HeaderComponent,
     VistasComponent,
     ListComponent,
-    TableComponent
+    TableComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
